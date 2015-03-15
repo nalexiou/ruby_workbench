24c31d7e688331516e69931279f96e78c23cfa05
Nikolaos Alexiou <nikolaos.alexiou@gmail.com>
2015-03-14 18:29:36 -0700

    Update eloquent_ruby.rb

--- eloquent_ruby.rb
+++ eloquent_ruby.rb
@@ -5,7 +5,18 @@ poem_words = %w{ twinkle little star how I wonder }
 
 #multiple arguments passed - position independent)
 def echo_at_least_two( first_arg, *middle_args, last_arg )
-puts "The first argument is #{first_arg}"
-middle_args.each { |arg|puts "A middle argument is #{arg}" }
-puts "The last argument is #{last_arg}"
+  puts "The first argument is #{first_arg}"
+  middle_args.each { |arg|puts "A middle argument is #{arg}" }
+  puts "The last argument is #{last_arg}"
+end
+
+#this works when names is an array
+def add_authors( names )
+  @author += " #{names.join(' ')}"
+end
+
+#this is better - splat operation (Exploding the array)
+
+def add_authors( *names )
+  @author += " #{names.join(' ')}"
 end
