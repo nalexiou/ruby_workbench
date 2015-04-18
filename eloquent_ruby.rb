87af5226776c43fd0a8fba56c14a1475eb024ece
Nikolaos Alexiou <nikolaos.alexiou@gmail.com>
2015-04-18 19:12:20 -0400

    hooks class and subclass example

--- eloquent_ruby.rb
+++ eloquent_ruby.rb
@@ -981,3 +981,56 @@ class ChildClassOne < SimpleBaseClass
 end
 
 #this will print: Hey ChildClassOne is now a subclass of SimpleBaseClass!
+
+class PlainTextReader < DocumentReader
+	def self.can_read?(path)
+		/.*\.txt/ =~ path
+	end
+	def initialize(path)
+		@path = path
+	end
+	def read(path)
+		File.open(path) do |f|
+			title = f.readline.chomp
+			author = f.readline.chomp
+			content = f.read.chomp
+			Document.new( title, author, content )
+		end 
+	end
+end
+
+class XMLReader < DocumentReader
+	def self.can_read?(path)
+		/.*\.xml/ =~ path
+	end
+	def initialize(path)
+		@path = path
+	end
+	def read(path)
+	    # Lots of complicated XML stuff omitted
+	end 
+end
+
+class DocumentReader
+  class << self
+    attr_reader :reader_classes
+end
+  @reader_classes = []
+	def self.read(path)
+		reader = reader_for(path)
+		return nil unless reader
+		reader.read(path)
+	end
+
+	def self.reader_for(path)
+	    reader_class = DocumentReader.reader_classes.find do |klass|
+	      klass.can_read?(path)
+	    end
+		return reader_class.new(path) if reader_class
+		nil 
+	end
+  #this will generate the reader_classes whenever a subclass is created	
+	def self.inherited(subclass)
+		DocumentReader.reader_classes << subclass
+	end
+end
