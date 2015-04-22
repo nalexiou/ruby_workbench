c23c365e6d14b080df9878a4bdc9bfd30c6e4692
Nikolaos Alexiou <nikolaos.alexiou@gmail.com>
2015-04-22 08:53:34 -0400

    method_missing suggestions

--- eloquent_ruby.rb
+++ eloquent_ruby.rb
@@ -1068,3 +1068,27 @@ class RepeatBackToMe
 	    puts "But there ain't no such method"
 	end 
 end
+
+#implementation that suggests missing method using Text gem
+require 'text'  # From the text gem
+
+class Document
+	include Text
+
+	# Most of the class omitted...
+	def method_missing( missing, *args )
+		candidates = methods_that_sound_like( missing.to_s )
+		message = "You called an undefined method: #{missing}."
+	unless candidates.empty?
+	  message += "\nDid you mean #{candidates.join(' or ')}?"
+	end
+	raise raise NoMethodError.new( message )
+	end
+	def methods_that_sound_like( name )
+		missing_soundex = Soundex.soundex( name.to_s )
+		public_methods.sort.find_all do |existing|
+		  existing_soundex = Soundex.soundex( existing.to_s )
+		  missing_soundex == existing_soundex
+		end
+	end 
+end
