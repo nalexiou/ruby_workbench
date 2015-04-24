406e7bf4976ab758b53e01a79cc65f7111eaf20d
Nikolaos Alexiou <nikolaos.alexiou@gmail.com>
2015-04-24 11:29:25 -0400

    method_missing and deligation

--- eloquent_ruby.rb
+++ eloquent_ruby.rb
@@ -1105,3 +1105,36 @@ class Document
 		raise msg 
 	end
 end
+
+#method delication using method_missing
+class SuperSecretDocument
+	def initialize(original_document, time_limit_seconds)
+		@original_document = original_document
+		@time_limit_seconds = time_limit_seconds
+		@create_time = Time.now
+	end
+	def time_expired?
+		Time.now - @create_time >= @time_limit_seconds
+	end
+	def check_for_expiration
+		raise 'Document no longer available' if time_expired?
+	end
+	def method_missing(name, *args)
+		check_for_expiration
+		@original_document.send(name, *args)
+	end
+end
+
+#being explicit
+
+class SuperSecretDocument
+  # Lots of code omitted...
+	DELEGATED_METHODS = [ :content, :words ]
+	def method_missing(name, *args) check_for_expiration
+		if DELEGATED_METHODS.include?( name )
+			@original_document.send(name, *args)
+		else
+			super 
+		end
+	end 
+end
