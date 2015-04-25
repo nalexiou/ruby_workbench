5a51c641bca08fb32169e4d13b9361b0856edf14
Nikolaos Alexiou <nikolaos.alexiou@gmail.com>
2015-04-25 00:20:34 -0400

    basicobject deligation

--- eloquent_ruby.rb
+++ eloquent_ruby.rb
@@ -1138,3 +1138,23 @@ class SuperSecretDocument
 		end
 	end 
 end
+
+
+class SuperSecretDocument < BasicObject
+# Most of the class omitted...
+	def initialize(original_document, time_limit_seconds)
+		@original_document = original_document
+		@time_limit_seconds = time_limit_seconds
+		@create_time = ::Time.now
+	end
+	def time_expired?
+		::Time.now - @create_time >= @time_limit_seconds
+	end
+	def check_for_expiration
+		raise 'Document no longer available' if time_expired?
+	end
+  def method_missing(name, *args)
+    check_for_expiration
+	 @original_document.send(name, *args)
+  end
+end
