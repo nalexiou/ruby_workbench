2b86acc3d3b479e1f6cff090485b24b7ed5b75cd
Nikolaos Alexiou <nikolaos.alexiou@gmail.com>
2015-04-27 23:38:07 -0400

    method_missing magic

--- eloquent_ruby.rb
+++ eloquent_ruby.rb
@@ -1160,3 +1160,38 @@ class SuperSecretDocument < BasicObject
 end
 
 #method_missing for flexible APIs
+
+#one implementation
+class FormLetter < Document
+  def replace_word( old_word, new_word )
+    @content.gsub!( old_word, "#{new_word}" )
+  end
+end
+
+offer_letter = FormLetter.new( "Special Offer", "Acme Inc",
+%q{
+    Dear Mr. LASTNAME
+    Are you troubled by the heartache of hangnails?
+    ...
+    FIRSTNAME, we look forward to hearing from you.
+})
+offer_letter.replace_word( 'FIRSTNAME', 'Russ' )
+offer_letter.replace_word( 'LASTNAME', 'Olsen' )
+
+#here is some magic with method_missing
+
+class FormLetter < Document
+	def replace_word( old_word, new_word )
+		@content.gsub!( old_word, "#{new_word}" )
+	end
+	def method_missing( name, *args )
+		string_name = name.to_s
+		return super unless string_name =~ /^replace_\w+/
+		old_word = extract_old_word(string_name)
+		replace_word( old_word, args.first )
+	end
+	def extract_old_word( name )
+		name_parts = name.split('_')
+		name_parts[1].upcase
+	end 
+end
