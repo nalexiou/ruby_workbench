f9e55c1a55f3df09bfb49c5aa077a6514f8394ba
Nikolaos Alexiou <nikolaos.alexiou@gmail.com>
2015-05-09 13:15:35 -0400

    parse statement with regex

--- eloquent_ruby.rb
+++ eloquent_ruby.rb
@@ -1477,3 +1477,26 @@ def parse_statement( statement )
 		raise "Unknown keyword: #{tokens.first}"
 	end 
 end
+
+#parse statement with regex
+def parse_statement( statement )
+	statement = statement.sub( /#.*/, '' )
+	case statement.strip
+	when ''
+	# Skip blank lines
+	when /print\s+'(.*?)'/ @ripper.on_path( $1 ) do |el|
+		puts el.text
+	end
+	when /delete\s+'(.*?)'/
+		@ripper.on_path( $1 ) { |el| el.remove }
+	when /replace\s+'(.*?)'\s+'(.*?)'$/ @ripper.on_path( $1 ) { |el| el.text = $2 }
+	when /uppercase\s+'(.*?)'/
+		@ripper.on_path( $1 ) { |el| el.text = el.text.upcase }
+	when /print_document/
+		@ripper.after do |doc|
+		puts doc 
+		end
+	else
+		raise "Don't know what to do with: #{statement}"
+	end
+end
