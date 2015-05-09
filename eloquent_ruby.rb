64300153361e348a94f853e3a5838ffa42fdd0b3
Nikolaos Alexiou <nikolaos.alexiou@gmail.com>
2015-05-09 13:06:03 -0400

    error messages

--- eloquent_ruby.rb
+++ eloquent_ruby.rb
@@ -1448,3 +1448,32 @@ class EzRipper
 		end 
 	end
 end
+
+#adding some error messages
+
+def parse_statement( statement )
+	tokens = statement.strip.split
+	return if tokens.empty?
+	case tokens.first
+	when 'print'
+		raise "Expected print <xpath>" unless tokens.size == 2
+		@ripper.on_path( tokens[1] ) do |el|
+			puts el.text
+		end
+	when 'delete'
+		raise "Expected delete <xpath>" unless tokens.size == 2
+		@ripper.on_path( tokens[1] ) { |el| el.remove }
+	when 'replace'
+		unless tokens.size == 3
+		raise "Expected replace <xpath> <value>"
+	end
+		@ripper.on_path( tokens[1] ) {|el| el.text = tokens[2]}
+	when 'print_document'
+		raise "Expected print_document" unless tokens.size == 1
+		@ripper.after do |doc|
+			puts doc
+		end
+	else
+		raise "Unknown keyword: #{tokens.first}"
+	end 
+end
